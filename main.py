8203499042a0564ebdba11436924447ebaefd39c
max_im_86 <maxim8686866@gmail.com>
2020-03-23 18:15:02 +0300

    clear

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ import mysql.connector
 import requests
 import pytesseract as tess
 from bs4 import BeautifulSoup as BS
-from datetime import datetime
 from telegram import Bot
 from PIL import Image
 from selenium import webdriver
 import os
+from selenium.webdriver.chrome.options import Options
 
 new = []
 vip = []
@@ -18,15 +18,11 @@ class ParseMode(object):
     HTML = 'HTML'
     """:obj:'str': 'HTML """
 
-
-
-
+# отправка сообщений
 def send_message(text):
-
     bot = Bot(
         token='1073700436:AAGRNtbf2_gW7W5WxehQn8uVUVRzqW1UGKI',
         base_url='https://telegg.ru/orig/bot',
-
     )
     bot.send_message(
         chat_id=325363221,
@@ -35,24 +31,20 @@ def send_message(text):
         disable_web_page_preview=True,
     )
 
-
+# соедение с таблицей
 def connect():
     db = mysql.connector.connect(
-        # host="127.0.0.1",
+        host="127.0.0.1",
         user="rooter",
         passwd="17721338",
         port="3306",
-        database="aviator")
+        database="aviator",
+        charset='utf8'
+    )
     return db
 
-
-
-# CREATE USER 'rooter'@'localhost' IDENTIFIED BY '17721338'
-# GRANT ALL PRIVILEGES ON *.* TO 'rooter'@'localhost'
-# SHOW GRANTS FOR 'rooter'@'localhost'
-# SHOW GRANTS FOR 'aviator'@'localhost'
-
-def stav():
+# парсинг страницы поиска
+def soups():
     session = requests.Session()
     request = session.get('https://www.avito.ru/moskva_i_mo/kvartiry/sdam/'
                           'na_dlitelnyy_srok-ASgBAgICAkSSA8gQ8AeQUg?cd=1&user=1&f=ASgBAQICAkSSA8gQ8AeQUgFAzAhEklmQWY5ZjFk',
@@ -62,31 +54,8 @@ def stav():
     divs = soup.find_all('div', class_='item__line')
     return divs
 
-
-def login():
-    driver = webdriver.Chrome()
-    driver.get('https://www.avito.ru')
-    driver.find_element_by_class_name(
-        'header-services-menu-link-TRWKh.header-services-menu-link-not-authenticated-3kAga').click()
-    driver.find_element_by_class_name(
-        'social-network-item-icon-1tXjO.social-network-item-icon_gp-30vPn').click()
-    time.sleep(3)
-    handles = driver.window_handles
-    for handle in handles:
-        driver.switch_to.window(handle)
-    driver.find_element_by_xpath('//*[@id="identifierId"]').send_keys('maxim8686866@gmail.com')
-    driver.find_element_by_xpath('//*[@id="identifierNext"]/span/span').click()
-    time.sleep(3)
-    driver.find_element_by_xpath('//*[@name="password"]').send_keys('17721338_aA_aA')
-    driver.find_element_by_xpath('//*[@id="passwordNext"]/span/span').click()
-    time.sleep(5)
-
-    driver.switch_to.window(
-        'https://www.avito.ru/moskva_i_mo/kvartiry/sdam/na_dlitelnyy_srok-ASgBAgICAkSSA8gQ8AeQUg?cd=1&user=1&f=ASgBAQICAkSSA8gQ8AeQUgFAzAhEklmQWY5ZjFk')
-    time.sleep(10)
-
-
-def stav_2(hrefs):
+# функция извлечения просмотров с страницы объявления
+def procmotr(hrefs):
     session = requests.Session()
     request = session.get(hrefs,
                           headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36'
@@ -99,31 +68,36 @@ def stav_2(hrefs):
         sees.append(x.text)
     return sees[0][17::]
 
+# создание файла
+def file():
+    path = os.path.dirname(__file__)
+    path = os.path.join(path, 'test.png')
+    return path
 
-def img(hrefs):
-    driver = webdriver.Chrome()
+# парсинг телефона с страницы объявления
+def telefon(hrefs):
+    chrome_options = Options()
+    chrome_options.add_argument('--headless')
+    driver = webdriver.Chrome(options=chrome_options)
     driver.get(hrefs)
     driver.find_element_by_partial_link_text('8 9').click()
     time.sleep(3)
-    # driver.find_element_by_class_name('item-phone-big-number.js-item-phone-big-number').screenshot('/Users/mac/Desktop/test.png')
     try:
-        driver.find_element_by_xpath('/html/body/div[11]/div/div/div/div/div[1]').screenshot(
-            '/Users/mac/Desktop/test.png')
+        driver.find_element_by_xpath('/html/body/div[11]/div/div/div/div/div[1]').screenshot(file())
     except:
         return "Не удалось скопировать номер"
-    img = Image.open('/Users/mac/Desktop/test.png')
-
+    img = Image.open(file())
     text = tess.image_to_string(img)
-    os.remove('/Users/mac/Desktop/test.png')
+    os.remove(file())
     return text
 
-
-def stav_1():
+# основной код
+def osnova():
     count = 0
     count_1 = 0
     db = connect()
     cursor = db.cursor()
-    for div in stav():
+    for div in soups():
         premium = div.find_all('div', attrs={'data-props': '{"vas":[]}'})
         if (len(premium)) == 0:
             vip.append(div)
@@ -156,7 +130,7 @@ def stav_1():
                 db.commit()
                 # if city == 'Москва':
                 send_message(
-                    f'<i>{city}</i> {kv[0]} {price[0]} <a href="{hrefs}">ссылка</a> {stav_2(hrefs)} {img(hrefs)}')
+                    f'<i>{city}</i> {kv[0]} {price[0]} <a href="{hrefs}">ссылка</a> {procmotr(hrefs)} {telefon(hrefs)}')
                 count_1 += 1
     if count_1 > 0:
         print('Добавилось ' + str(count_1) + ' объявлений по МСК')
@@ -164,9 +138,10 @@ def stav_1():
         print('Добавилось ' + str(count - count_1) + ' объявлений по МО')
 
 
-# login()
-while True:
-    stav_1()
-
-    time.sleep(10)
-# print(datetime.now() - start_time)
+if __name__ == '__main__':
+    while True:
+        try:
+            osnova()
+        except ConnectionError:
+            pass
+        time.sleep(10)
